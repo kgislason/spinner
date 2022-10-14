1d61f1da2844bd6e72c1f254159b3e366b9fa34d
Kristy Gislason <kristygislason@gmail.com>
2022-10-14 16:20:42 -0700

    Add README.md, clean up spinner2

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spinner
+
+A low-fidelity loading spinner using setTimeout.
+
+# Installation
+
+`git clone git@github.com:kgislason/spinner.git`
+`cd spinner`
+`node spinner1.js`
+`node spinner2.js`
--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,7 @@
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 
 const spinner = (start, inc) => {
-
   const animation = ['\r|   ', '\r/   ', '\r-   ', '\r\\   '];
-
   for (let step of animation) {
     setTimeout(() => {
       process.stdout.write(step);
